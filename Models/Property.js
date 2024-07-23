a6e8201f0f5cc619e15dabdc5e63b56fd3a54aa6
tariqrasheed172001 <rasheedhadiq@gmail.com>
2024-07-24 00:08:40 +0530

    updating the propertySchema

--- Models/Property.js
+++ Models/Property.js
@@ -2,13 +2,15 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const PropertySchema = new Schema({
-    owner: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
     title: { type: String, required: true },
     description: { type: String, required: true },
     location: { type: String, required: true },
+    bhk:{type:String, required: true},
+    area: {type:String, required: true},
     price: { type: Number, required: true },
     amenities: [String],
     photos: [String],
+    popular:{type: Boolean}
 }, { timestamps: true });
 
 const PropertyModel = mongoose.model('Property', PropertySchema);
