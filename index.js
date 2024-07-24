d126b672e0ec8eb24baa0985d9b011819c044352
tariqrasheed172001 <rasheedhadiq@gmail.com>
2024-07-24 20:28:33 +0530

    add test route

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 // Test route
 app.get('/test', (req, res) => {
-  console.log("server is running fine!");
+  successLogger.http("server is running fine!");
   res.status(200).json({ message: "Server is running fine!" });
 });
 
